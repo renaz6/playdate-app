7be4aae0d71fdc3f934e37f65a50e25341fa95e3
Serena Zamarripa <rena.zamarripa@gmail.com>
2020-04-03 00:02:32 -0500

    Skeleton of Event Details Page, Bug with pulling up screen twice

--- playdate-app/MyEventsViewController.swift
+++ playdate-app/MyEventsViewController.swift
@@ -16,7 +16,6 @@ class MyEventsViewController: UIViewController, UITableViewDataSource, UITableVi
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         dataSource = MockDataSource()
     }
     
